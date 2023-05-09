d39c95a8357403ec44b5f3b93ce1eb739595ab2c
Vladimus <petroniodasilvafigueiredo@gmail.com>
2023-05-08 22:29:32 -0300

    Console app

---
+++ First/myConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void main(string[] arg)
+        {
+            Console.Clear();
+            Console.WriteLine("Hello, World");
+
+        }
+    }
+
+}
