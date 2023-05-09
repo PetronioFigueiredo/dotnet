7579121b1dde42ca3a1fbca88feb7d0f5b12b720
Vladimus <petroniodasilvafigueiredo@gmail.com>
2023-05-08 21:38:36 -0300

    dotnet first try

---
+++ First/myfirstspmvcapp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace myfirstspmvcapp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
